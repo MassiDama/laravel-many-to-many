87cfdd1de353a24c9b083060a9cbcfcf8d8a7875
Massidama <massidama@outlook.it>
2024-02-28 17:57:47 +0100

    chiamata axios in front

---
+++ front/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import ApiContent from './components/ApiContent.vue';
+
+export default{
+    components:{
+      ApiContent,
+    }
+  }
+
+</script>
+
+<template>
+
+  <ApiContent/>
+  
+</template>
+
+<style>
+
+</style>
---
+++ front/src/components/ApiContent.vue
@@ -0,0 +1,42 @@
+<script>
+import axios from "axios";
+
+export default{
+        name: 'ApiContent',
+        data(){
+            return{
+                technologies: [],
+            }
+        },
+        mounted() {
+
+            axios.get("http://localhost:8000/api/v1/technologies")
+                .then((res) => {
+                    
+                    const data = res.data;
+                    console.log('data: ', data);
+
+                    this.technologies = res.data.data;
+                    console.log('techno:', this.technologies);
+
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+    },
+    }
+
+</script>
+
+<template>
+  <h1>Technologies</h1>
+  <ul>
+    <li v-for="technology in technologies" :key="technology.id" >
+        {{ technology.technology_name }}
+    </li>
+  </ul>
+</template>
+
+<style>
+
+</style>
